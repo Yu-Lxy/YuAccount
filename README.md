dc8e86bc670e5c33975b86f569b461a260d6e531
lixiaoyu <1154478076@qq.com>
2023-02-14 18:20:19 +0800

    确定框架

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # YuAccount
 小鱼记账🧾
+
+## client: vue3
+### 启动：yarn serve
+## server: egg
+### 启动：npm run dev
---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>haha</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
